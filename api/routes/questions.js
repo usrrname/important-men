0179cf0cbd2a35caeb2f97859ef3069c0efae8b3
usrrname <usrrname@users.noreply.github.com>
2017-10-21 23:45:47 -0400

    sendgrid works! posts twice though

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -1,31 +1,31 @@
 //this file is questions/whatever-endpoint 
+const express = require('express');
+const dB = require('../mongo');
+const questionsRouter = express.Router();
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var dB = require('../mongo');
-var questionsRouter = express.Router();
-var bodyParser = require('body-parser');
+//for nodemailer & sendgrid
+const sgMail = require('@sendgrid/mail');
+const nodemailer = require('nodemailer');
+const SENDGRID_API_KEY = require('../../sendgrid.env');
+const config = require('../config');
+const email = process.env.EMAIL || config.email;
 
 //'questions/' endpoint hit when user selects to see all q+a
 questionsRouter.get('/', (req, res) => {
   const db = dB.get();
   const collection = db.collection('questions');
   collection.find({}).toArray((err, resp) => {
-    if (err) { throw err; 
-}
+    if (err) {
+      throw err;
+    }
     res.json(resp);
   });
 });
-  //res.send('send all the questions with responses here');
-  //mock data
-  // res.json([{ "name": "john", "email":"john@jones.com", "comments": "Hey Girl?"},
-  // { "name": "alex", "email":"help@alex.com", "comments": "What is going on?"},
-  // { "name": "kate", "email":"k@lost.com", "comments": "Where is my hairbrush?"}
-  // ]);
-
 
 questionsRouter.post('/ask', (req, res) => {
   console.log('question received');
-  console.log(req.body);
+  //console.log(req.body);
   const db = dB.get();
   const collection = db.collection('questions');
   collection.insert(req.body, (err, result) => {
@@ -36,6 +36,24 @@ questionsRouter.post('/ask', (req, res) => {
       console.log(result);
     }
   });
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: 'questions@importantmen.com',
+    subject: 'Hello World!',
+    text: 'My first email through SendGrid',
+    html: `<p>You have a new question.</p>
+    <h3>Contact Details<h/3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.comment}</p>
+    `,
+  };
+  sgMail.send(msg);
+  console.log('question handled by sendgrid');
   // res.send('you sent a question');
   return res.status;
 });
