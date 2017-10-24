1b91a3f488286edd31a7bc0b0ae6aa238e86a791
usrrname <usrrname@users.noreply.github.com>
2017-10-23 21:59:47 -0400

    find by objectID mystery continues, posting new nulls to mongo LOL

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -10,8 +10,7 @@ const nodemailer = require('nodemailer');
 const SENDGRID_API_KEY = require('../../sendgrid.env');
 const config = require('../config');
 const email = process.env.EMAIL || config.email;
-const mongo = require('mongodb');
-const ObjectID = require('bson-objectid'); //for accessing ObjectID()
+const ObjectID = require('mongodb').ObjectID;//for accessing ObjectID()
 
 //'questions/' endpoint hit when user selects to see all q+a
 questionsRouter.get('/', (req, res) => {
@@ -27,7 +26,6 @@ questionsRouter.get('/', (req, res) => {
 
 questionsRouter.post('/ask', (req, res) => {
   console.log('question received');
-  //console.log(req.body);
   const db = dB.get();
   const collection = db.collection('questions');
   collection.insert(req.body, (err, result) => {
@@ -37,7 +35,7 @@ questionsRouter.post('/ask', (req, res) => {
       console.log('inserted');
       const hex = req.body['_id'] + ''; 
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const id = req.body['_id'];
+      const id = req.params['_id'];
       const msg = {
         to: email,
         from: 'questions@importantmen.com',
@@ -46,25 +44,24 @@ questionsRouter.post('/ask', (req, res) => {
         html: `<p>You have a new question!</p>
               <h3>Contact Details<h/3>
               <ul>
-              <li>Name: ${req.body.name}</li>
-              <li>Email: ${req.body.email}</li>
-              <li>Mongo ID: ${hex} </li>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+                <li>Mongo ID: ${hex} </li>
               </ul>
               <h3>Message</h3>
-              <p>${req.body.comment}</p>
+                <p>${req.body.comment}</p>
               <br><hr><br>
               <h3>Respond in this form below</h3>
               <form id="res-form"  action="http://localhost:3000/questions/response/" method="post">
-              <input id='fromAsk' type='hidden' value="${id}">
+                  <input id='fromAsk' type='hidden' value="${id}">
                 <label>Subject Line</label><br>
-                <input id='title' name='title' placeholder='subject line' type='text'><br>
+                  <input id='title' name='title' placeholder='subject line' type='text'><br>
                 <label>Your Answer</label><br>
-                <textarea id='advice' name='advice' type='text' placeholder='Enter Your Lovely Advice :)'></textarea><br>
+                  <textarea id='advice' name='advice' type='text' placeholder='Enter Your Lovely Advice :)'></textarea><br>
                 <div>
                   <input id="answerButton" type="submit" value="Submit">
                 </div>
-              </form>
-      `,
+              </form>`,
       };
       sgMail.send(msg);
       console.log(msg);
@@ -78,29 +75,22 @@ questionsRouter.post('/ask', (req, res) => {
 questionsRouter.post('/response', (req, res, err) => {
   const db = dB.get();
   const collection = db.collection('questions');
-  const s = req.body.fromAsk.toArray();
-  console.log(s);
-  const safeObjectId = s => ObjectID.isValid(s) ? new ObjectID(s) : null;
-  const id = s;
+  const _id = req.body.fromAsk;
+  console.log(_id);
+  const selectParas = { "_id": _id}; 
+  const updateValues = { answerTitle: `${req.body.title}`, advice: `${req.body.advice}` };
+
   collection.findOneAndUpdate(
-    { _id: safeObjectId(id) },
-    { $addToSet: { answerTitle: `${req.body.title}`, advice: `${req.body.advice}` } },
-    { returnOriginal: true }
+    selectParas,
+    { $addToSet: updateValues },
+    { upsert: true, returnNewDocument: true },
   )
-    .then( (result, error) => {
+    .then((result, error) => {
       if (error) {
-        console.log(error); 
+        console.log(error);
       }
       console.log(result);
       res.send('Your response was submitted to the Matthieu database')
     });
-  // const o_id = ObjectID.createFromHexString(hexString);
-  // collection.findOneAndUpdate(
-  //   { _id: {$eq: [o_id] } }, //query
-  //   // { _id: { $eq: db.ObjectId(o_id) } }, //query
-  //   { $addToSet: { answerTitle: `${req.body.title}`, advice: `${req.body.advice}` } }, //option of addToSet
-  //   { upsert: true, returnNewDocument: true },
-  // );
-  // console.log(err);
 });
 module.exports = questionsRouter;
